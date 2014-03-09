e7ff0451c08adc3cadb907b4a8478289dc932fb4
entei <pavlovskes@gmail.com>
2014-03-09 18:55:32 +0300

    some fix

--- ipControl/app/models/record.rb
+++ ipControl/app/models/record.rb
@@ -22,13 +22,14 @@ class Record < ActiveRecord::Base
     network = IPAddress::IPv4.new wan
     hosts = network.hosts.map { |i| i.to_s } # all hosts in network
     sub = IPAddress::IPv4.new ip
-    # so shit dude 
-    if sub.hosts.count > 0
+    # Количество хостов в подсети определяется как 2^(32-N)-2, где N — длина маски. 
+    return false if sub.prefix < network.prefix
+    if sub.prefix == 32
+      hosts.include? sub.address 
+    else
       subnet = sub.hosts.map { |e| e.to_s  }
       (subnet - hosts).size == 0 # all subnet elements must be in the host array
-    else
-      hosts.include? sub.address
-    end
+    end        
   rescue
    errors.add(edge, 'неверно задан ip-адрес')
   end
